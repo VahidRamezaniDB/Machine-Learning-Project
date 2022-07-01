d478d3703be8bba8fc78eb4bf248227a885ec300
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-07-01 12:27:19 +0430

    main routine implemented

--- clustering_alg.py
+++ clustering_alg.py
@@ -73,53 +73,144 @@ print("Y_test: " + str(Y_test.shape))
 
 ### CLUSTRING ALGORITHM
 
-# Decleration of the algorithm's parameters.
-DESIRED_CLUSTERS = 4
-CONSTANT_NUMBER_G = 2
-CONSTATN_NUMBER_K = 5
-
-## 1
-# Computing distance matrix(D_original).
-D_original = pd.DataFrame(distance_matrix(X_train.values,X_train.values), index=list(range(X_train.shape[0])), columns=list(range(X_train.shape[0])))
-
-##2
-# Finding K-Nearest data points.
-K = CONSTATN_NUMBER_K
-R_k = []
-for idx ,x in D_original.iterrows():
-    R_k.append(list(x.nsmallest(K + 1).keys()))
-R_k = pd.DataFrame(R_k)
-
-##3
-# Initializing cluster labels.
-N = X_train.shape[0]
-L = list(range(0,N))
-G = CONSTANT_NUMBER_G
-
-##4
-# Initializing algorithm params.
-C_previous = N
-C_current = math.floor(N/G)
+# Identify key element.
+# This function is called in each iteration to identify key elements of the new clusters.
+def identify_key_elements(d_current: pd.DataFrame, C_current: int)->pd.DataFrame:
+    pass
 
 
-##5
-# Initial Computation of D_current 
-D_current = np.zeros((N,N))
-D_org_list = D_original.values.tolist()
-R_k_list = R_k.values.tolist()
-k2 = (K + 1)**2
-for i in range(N):
-    for j in range(i, N):
-        sum_val = 0
-        R_k_i = R_k_list[i]
-        R_k_j = R_k_list[j]
-        for h in range(K):
-            for p in range(K):                
-                sum_val += D_org_list[R_k_i[h]][R_k_j[p]]
-        dist = sum_val/(k2)
-        D_current[i][j] = dist
-        D_current[j][i] = dist
-D_current = pd.DataFrame(D_current)
-print("D_current: ", D_current.shape)
-print(D_current.head())
+# This is the main routine of the algorithm.
+def custom_clustering(X_train: pd.DataFrame)-> pd.DataFrame:
 
+    # Decleration of the algorithm's parameters.
+    DESIRED_CLUSTERS = 4
+    CONSTANT_NUMBER_G = 2
+    CONSTATN_NUMBER_K = 5
+
+    ## 1
+    # Computing distance matrix(D_original).
+    D_original = pd.DataFrame(distance_matrix(X_train.values,X_train.values), index=list(range(X_train.shape[0])), columns=list(range(X_train.shape[0])))
+
+    ##2
+    # Finding K-Nearest data points.
+    K = CONSTATN_NUMBER_K
+    R_k = []
+    for idx ,x in D_original.iterrows():
+        R_k.append(list(x.nsmallest(K + 1).keys()))
+    R_k = pd.DataFrame(R_k)
+
+    ##3
+    # Initializing cluster labels.
+    N = X_train.shape[0]
+    L = list(range(0,N))
+    G = CONSTANT_NUMBER_G
+
+    ##4
+    # Initializing algorithm params.
+    C_previous = N
+    C_current = math.floor(N/G)
+    C_target = DESIRED_CLUSTERS
+
+    ##5
+    # Initial Computation of D_current 
+    D_current = np.zeros((N,N))
+    D_org_list = D_original.values.tolist()
+    R_k_list = R_k.values.tolist()
+    k2 = (K + 1)**2
+    for i in range(N):
+        for j in range(i, N):
+            sum_val = 0
+            R_k_i = R_k_list[i]
+            R_k_j = R_k_list[j]
+            for h in range(K):
+                for p in range(K):                
+                    sum_val += D_org_list[R_k_i[h]][R_k_j[p]]
+            dist = sum_val/(k2)
+            D_current[i][j] = dist
+            D_current[j][i] = dist
+    D_current = pd.DataFrame(D_current)
+    print("D_current: ", D_current.shape)
+    print(D_current.head())
+
+    ##6
+    # Main loop of the algorithm
+
+    while(C_current > C_target):
+        S_current = identify_key_elements(D_current, C_current)
+
+        S_current_list = S_current[S_current.columns[0]].tolist()
+        D_current_list = D_current.values.tolist()
+
+        # Re-assigning cluster label for each data point
+        for i in range(len(L)):
+            if i not in S_current_list:
+                # min = math.inf
+                # for j in D_current_list[i]:
+                #     if j in S_current_list and j < min:
+                #         min = j
+                temp = list(D_current_list[i])
+                temp.sort(reverse=True)
+                while(len(temp)>0):
+                    min_d = temp.pop()
+                    if min_d in S_current_list:
+                        L[i] = min_d
+                        break
+                temp.clear()
+
+        ## Re-computing D_current
+        D_current = np.zeros((C_current, C_current))
+        D_org_list = D_original.values.tolist()
+        R_k_list = R_k.values.tolist()
+
+        # Computing "P"s. "P"s contains all data points in a certain cluster and all their neighbours.
+        P = []
+        for i in range(C_current):
+            P_i = []
+            for idx in range(len(L)):
+                if L[idx] == i:
+                    P_i.append(idx)
+                    P_i.extend(R_k_list[idx])
+            P_i = list(set(P_i))
+            P.append(P_i)
+        
+        # Computing new D_current values
+        for i in range(C_current):
+            for j in range(i, C_current):
+                sum_val = 0
+                P_i = P[i]
+                P_j = P[j]
+                len_P_i = len(P_i)
+                len_P_j = len(P_j)
+                for h in range(len(P_i)):
+                    for q in range(len(P_j)):
+                        sum_val += D_org_list[P_i[h]][P_j[q]]
+                dist = sum_val/(len_P_i*len_P_j)
+                D_current[i][j] = dist
+                D_current[j][i] = dist
+        D_current = pd.DataFrame(D_current)
+
+        # Re-computing other parameters
+        C_previous = C_current
+        C_current = math.floor(C_current/G)
+    
+
+    ##7
+    # Cease condition satisfied.
+    S_current = identify_key_elements(D_current, C_current)
+
+    S_current_list = S_current[S_current.columns[0]].tolist()
+    D_current_list = D_current.values.tolist()
+
+    # Re-assigning cluster label for each data point
+    for i in range(len(L)):
+        if i not in S_current_list:
+            temp = list(D_current_list[i])
+            temp.sort(reverse=True)
+            while(len(temp)>0):
+                min_d = temp.pop()
+                if min_d in S_current_list:
+                    L[i] = min_d
+                    break
+            temp.clear()
+
+    return pd.DataFrame(L)
